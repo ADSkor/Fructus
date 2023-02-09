a25f5fd1bb35437cf6e7050e0f69485d569ec347
ADSkor <ADSkorotkin@gmail.com>
2023-02-09 18:12:41 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Aleksandr Skorotkin on 09.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
